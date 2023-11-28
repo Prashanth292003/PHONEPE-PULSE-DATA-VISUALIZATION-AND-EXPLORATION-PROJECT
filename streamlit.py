d1693ac4258c6f0e84865ba55a0cc208d1119fef
PRASHANTH K <123961512+Prashanth292003@users.noreply.github.com>
2023-11-28 11:48:23 +0530

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,44 @@
 import mysql.connector
 import streamlit as st
 import pandas as pd
-st.title('Phonepe Pulse Data Visualization and Exploration')
-ques= st.selectbox("Select the Question",('Select a Query','Top Brands Of Mobiles Used and transaction count','Type of transaction type, count, amount','States and Districts With Lowest Trasaction Amount',
+from PIL import Image
+# st.set_page_config(layout= "wide")
+st.set_page_config(page_title="Streamlit App", page_icon=":rocket:", layout="wide", initial_sidebar_state="expanded")
+
+logo_path = r"C:\Users\Senthil\Downloads\phonepeimage.png"
+image = Image.open(logo_path)
+st.image(image, width=300)
+
+video_path = "C:/Users/Senthil/Downloads/phonepead.mp4"
+css = '''
+<style>
+    .stTabs [data-baseweb="tab-list"] button [data-testid="stMarkdownContainer"] p {
+    font-size:1.5rem;
+    }
+</style>
+'''
+st.markdown(css, unsafe_allow_html=True)
+tab1, tab2, tab3, tab4 = st.tabs(["***HOME***&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", "***EXPLORE DATA***&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", "***DATA VISULAIZATION***&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", '***REPORTS***&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'])
+
+with tab1:
+  st.title("PHONEPE DATA VISUALIZATION AND EXPLORATION")
+with tab1:
+  col1,col2= st.columns(2)
+  with col2:
+    st.video(video_path)
+  with col1:
+      st.header("About PhonePe")
+      st.write("PhonePe, established in 2015, has swiftly emerged as a game-changer in India's digital financial realm. Operating on the Unified Payments Interface (UPI), it offers a diverse suite of services encompassing peer-to-peer transfers, bill payments, and seamless online shopping experiences. Its intuitive interface, fortified by stringent security measures, has catapulted PhonePe into the forefront of the country's digital payments landscape, catering to millions seeking a convenient and secure platform. Beyond transactions, PhonePe's commitment to inclusivity is evident through innovative solutions like PhonePe for Business, empowering merchants with digital payment acceptance. Under Flipkart's umbrella since 2016, PhonePe has evolved into a multifaceted financial services platform, extending its offerings to include investments, insurance, and travel bookings, marking its transition from a mere payment facilitator to a comprehensive financial ecosystem. Continuously evolving and embracing innovation, PhonePe's pivotal role in reshaping India's digital economy, fostering financial inclusion, and propelling the nation towards a cashless future remains undeniably significant.")
+      st.markdown("[Download PhonePe App](https://www.phonepe.com/app-download/)")
+      # st.markdown("[Project GitHub Link](https://github.com/Prashanth292003)")
+
+with tab2:
+  col1,col2= st.columns(2)
+  logo_path = r"C:\Users\Senthil\Downloads\answer.png"
+  image = Image.open(logo_path)
+  col1.image(image, width=300)
+  col2.title('EXPLORE HERE:')
+  ques= col2.selectbox("Select the Question",('Select a Query','Top Brands Of Mobiles Used and transaction count','Type of transaction type, count, amount','States and Districts With Lowest Trasaction Amount',
                                   'States and Districts With Highest Transaction Amount','Top 10 Districts and states With Lowest Transaction Amount','Top 10 Districts and states With highest Transaction Amount',
                                   'Top States With AppOpens','Top States With RegesteredUser','States and Districts With Lowest Trasaction Count',"Top 10 States With AppOpens",
                                  'States and Districts With Highest Transaction Count','Top Transaction count with States and pincodes','Least Transaction count with States and pincodes',
@@ -25,13 +61,13 @@ distinct_states = cursor.fetchall()
 cursor.close()
 connection.close()
 state_options = [state[0] for state in distinct_states]
-
-selected_option = st.selectbox("Select an years", ["Select","2018", "2019","2020","2021","2022","2023"])
-selected_option1 = st.selectbox("Select Quarter", ["Select","1", "2","3","4"])
-selected_option2 = st.selectbox("Select States",['Select State'] + state_options)
-Years = selected_option
-Quarter = selected_option1
-States = selected_option2
+with tab2:
+  selected_option = col2.selectbox("Select an years", ["Select","2018", "2019","2020","2021","2022","2023"])
+  selected_option1 = col2.selectbox("Select Quarter", ["Select","1", "2","3","4"])
+  selected_option2 = col2.selectbox("Select States",['Select State'] + state_options)
+  Years = selected_option
+  Quarter = selected_option1
+  States = selected_option2
 
 host = "localhost"
 user = "root"
@@ -57,12 +93,12 @@ def ques1():
   cursor.close()
   connection.close()
   return Ans      
-      
-if ques=="Top Brands Of Mobiles Used and transaction count":
-  if st.button("Top Brands Of Mobiles Used and transaction count"):
-    Ans = ques1()
-    st.write(Ans)
-    st.button("Clear")
+with tab2 and col2:       
+  if ques=="Top Brands Of Mobiles Used and transaction count":
+    if st.button("Top Brands Of Mobiles Used and transaction count"):
+      Ans = ques1()
+      col1.write(Ans)
+      col1.button("Clear")
 
 def ques2():
   sql_query2 = """
@@ -78,11 +114,12 @@ def ques2():
   connection.close()
   return Final
   
-if ques=='States and Districts With Highest Transaction Amount':
-  if st.button("States and Districts With Highest Transaction Amount"):
-    Finals = ques2()
-    st.write(Finals)
-    st.button("Clear")
+with tab2 and col2: 
+  if ques=='States and Districts With Highest Transaction Amount':
+    if st.button("States and Districts With Highest Transaction Amount"):
+      Finals = ques2()
+      col1.write(Finals)
+      col1.button("Clear")
     
     
 def ques3():
@@ -96,12 +133,13 @@ def ques3():
   cursor.close()
   connection.close()
   return Final
-  
-if ques=='Type of transaction type , count, amount':
-  if st.button("Type of transaction type , count, amount"):
-    Finals = ques3()
-    st.write(Finals)
-    st.button("Clear")
+
+with tab2 and col2:   
+  if ques=='Type of transaction type, count, amount':
+    if st.button("Type of transaction type , count, amount"):
+      Finals = ques3()
+      col1.write(Finals)
+      col1.button("Clear")
     
 def ques4():
   sql_query4 = """
@@ -116,12 +154,13 @@ def ques4():
   cursor.close()
   connection.close()
   return Final
-  
-if ques=='States and Districts With Lowest Trasaction Amount':
-  if st.button("States and Districts With Lowest Trasaction Amount"):
-    Finals = ques4()
-    st.write(Finals)
-    st.button("Clear") 
+
+with tab2 and col2:  
+  if ques=='States and Districts With Lowest Trasaction Amount':
+    if st.button("States and Districts With Lowest Trasaction Amount"):
+      Finals = ques4()
+      col1.write(Finals)
+      col1.button("Clear") 
       
       
 def ques5():
@@ -137,11 +176,12 @@ def ques5():
   connection.close()
   return Final
   
-if ques=='Top 10 Districts and states With Lowest Transaction Amount':
-  if st.button("Top 10 Districts and states With Lowest Transaction Amount"):
-    Finals = ques5()
-    st.write(Finals)
-    st.button("Clear")
+with tab2 and col2: 
+  if ques=='Top 10 Districts and states With Lowest Transaction Amount':
+    if st.button("Top 10 Districts and states With Lowest Transaction Amount"):
+      Finals = ques5()
+      col1.write(Finals)
+      col1.button("Clear")
     
     
     
@@ -157,12 +197,13 @@ def ques6():
   cursor.close()
   connection.close()
   return Final
-  
-if ques=='Top 10 Districts and states With highest Transaction Amount':
-  if st.button("Top 10 Districts and states With highest Transaction Amount"):
-    Finals = ques6()
-    st.write(Finals)
-    st.button("Clear")
+
+with tab2 and col2:   
+  if ques=='Top 10 Districts and states With highest Transaction Amount':
+    if st.button("Top 10 Districts and states With highest Transaction Amount"):
+      Finals = ques6()
+      col1.write(Finals)
+      col1.button("Clear")
     
     
     
@@ -178,12 +219,13 @@ def ques7():
   cursor.close()
   connection.close()
   return Final
-  
-if ques=='Top 10 States With AppOpens':
-  if st.button("Top 10 States With AppOpens"):
-    Finals = ques7()
-    st.write(Finals)
-    st.button("Clear")
+
+with tab2 and col2:    
+  if ques=='Top 10 States With AppOpens':
+    if st.button("Top 10 States With AppOpens"):
+      Finals = ques7()
+      col1.write(Finals)
+      col1.button("Clear")
 
 
 
@@ -201,12 +243,13 @@ def ques8():
   cursor.close()
   connection.close()
   return Final
-  
-if ques=='Top States With AppOpens':
-  if st.button("Top States With AppOpens"):
-    Finals = ques8()
-    st.write(Finals)
-    st.button("Clear")
+
+with tab2 and col2:    
+  if ques=='Top States With AppOpens':
+    if st.button("Top States With AppOpens"):
+      Finals = ques8()
+      col1.write(Finals)
+      col1.button("Clear")
     
     
     
@@ -225,11 +268,12 @@ def ques9():
   connection.close()
   return Final
   
-if ques=='Top States With RegesteredUser':
-  if st.button("Top States With RegesteredUser"):
-    Finals = ques9()
-    st.write(Finals)
-    st.button("Clear")
+with tab2 and col2: 
+  if ques=='Top States With RegesteredUser':
+    if st.button("Top States With RegesteredUser"):
+      Finals = ques9()
+      col1.write(Finals)
+      col1.button("Clear")
     
     
     
@@ -246,12 +290,13 @@ def ques10():
   cursor.close()
   connection.close()
   return Final
-  
-if ques=='States and Districts With Lowest Trasaction Count':
-  if st.button('States and Districts With Lowest Trasaction Count'):
-    Finals = ques10()
-    st.write(Finals)
-    st.button("Clear")
+
+with tab2 and col2:   
+  if ques=='States and Districts With Lowest Trasaction Count':
+    if st.button('States and Districts With Lowest Trasaction Count'):
+      Finals = ques10()
+      col1.write(Finals)
+      col1.button("Clear")
     
     
     
@@ -268,12 +313,13 @@ def ques111():
   cursor.close()
   connection.close()
   return Final
-  
-if ques=='States and Districts With Highest Transaction Count':
-  if st.button('States and Districts With Highest Transaction Count'):
-    Finals = ques111()
-    st.write(Finals)
-    st.button("Clear")
+
+with tab2 and col2:   
+  if ques=='States and Districts With Highest Transaction Count':
+    if st.button('States and Districts With Highest Transaction Count'):
+      Finals = ques111()
+      col1.write(Finals)
+      col1.button("Clear")
     
     
     
@@ -290,12 +336,13 @@ def ques12():
   cursor.close()
   connection.close()
   return Final
-  
-if ques=='Top Transaction count with States and pincodes':
-  if st.button('Top Transaction count with States and pincodes'):
-    Finals = ques12()
-    st.write(Finals)
-    st.button("Clear")
+
+with tab2 and col2:    
+  if ques=='Top Transaction count with States and pincodes':
+    if st.button('Top Transaction count with States and pincodes'):
+      Finals = ques12()
+      col1.write(Finals)
+      col1.button("Clear")
     
     
     
@@ -312,12 +359,13 @@ def ques13():
   cursor.close()
   connection.close()
   return Final
-  
-if ques=='Least Transaction count with States and pincodes':
-  if st.button('Least Transaction count with States and pincodes'):
-    Finals = ques13()
-    st.write(Finals)
-    st.button("Clear")
+
+with tab2 and col2:  
+  if ques=='Least Transaction count with States and pincodes':
+    if st.button('Least Transaction count with States and pincodes'):
+      Finals = ques13()
+      col1.write(Finals)
+      col1.button("Clear")
     
     
     
@@ -335,12 +383,13 @@ def ques14():
   cursor.close()
   connection.close()
   return Final
-  
-if ques=='Least RegisteredUser with States and pincodes':
-  if st.button('Least RegisteredUser with States and pincodes'):
-    Finals = ques14()
-    st.write(Finals)
-    st.button("Clear")
+
+with tab2 and col2:  
+  if ques=='Least RegisteredUser with States and pincodes':
+    if st.button('Least RegisteredUser with States and pincodes'):
+      Finals = ques14()
+      col1.write(Finals)
+      col1.button("Clear")
     
     
     
@@ -357,9 +406,31 @@ def ques15():
   cursor.close()
   connection.close()
   return Final
-  
-if ques=='Top RegisteredUser with States and pincodes':
-  if st.button('Top RegisteredUser with States and pincodes'):
-    Finals = ques15()
-    st.write(Finals)
-    st.button("Clear")
+
+with tab2 and col2:    
+  if ques=='Top RegisteredUser with States and pincodes':
+    if st.button('Top RegisteredUser with States and pincodes'):
+      Finals = ques15()
+      col1.write(Finals)
+      col1.button("Clear")
+      
+      
+      
+      
+      
+with tab4:
+  st.title("Comprehensive Report:")
+  col1, col2 = st.columns(2)
+  col1.header("Digital Payments in India: A US$10 Tn Opportunity!")
+  col1.write("Check out the new PhonePe Pulse - BCG report on what the future holds for digital payments in India.")
+  file_path = 'C:/Users/Senthil/Downloads/report.pdf'
+  with open(file_path, 'rb') as file:
+    file_content = file.read()
+  col1.download_button('Download Report', file_content, key='file_download', file_name='report.pdf')
+
+
+  logo_path = r"C:\Users\Senthil\Downloads\imp.png"
+  image = Image.open(logo_path)
+  # resized_image = image.resize((400, image.size[2]))
+  # col2.image(resized_image)
+  col2.image(image, width=650)
